5f4f831fb216fba800b44d247bfc20b3e5ef12f9
mads <madspbuch@gmail.com>
2013-02-10 21:26:13 +0100

    before pull

--- start/finance/layout/MailWelcome.class.php
+++ start/finance/layout/MailWelcome.class.php
@@ -13,42 +13,11 @@ class MailWelcome extends \helper\layout\LayoutBlock{
 	}
 	
 	public function generate(){
-		$link = \config\config::$configs['finance']['settings']['protocol'].'://'.\config\config::$configs['finance']['domains']['web'];
-		$codeLink = $link.'/index/activate/'.$this->user->activationKey.'/'.$this->user->_id;
-		return "
-<h1>Velkommen til finansmaskinen</h1>
-
-<h3>Du er nu oprettet som bruger på finansmaskinen.</h3>
-
-<p>Dit brugernavn er: {$this->user->mail}</p>
-
-<p>For at verificere din bruger vil vi bede dig om, at klikke på følgende link:</p>
-
-<a href=\"$codeLink\">$codeLink</a>
-
-<p>Når du har verificeret din mail vil du blive guidet hen til opsætningen af din bruger,
-her skal du udfylde flere informationer omkring din virksomhed.</p>
-
-<p>Hvis du ikke kan udfylde det hele på nuværende tidspunkt eller hvis du ønsker at ændre dit
-password, brugernavn eller andet, så kan du også gøre det når du er logget ind i systemet.</p>
-
-<h3>Priser, abonnement og betingelser:</h3>
-<p>Systemet er som udgangspunkt gratis for dig at benytte. Du får hver måned tildelt 5 gratis
-fakturaer samt regninger. Når disse er opbrugt, vil det koste dig et mindre beløb at sende
-en faktura eller at registrere en regning. Du kan på vores hjemmeside få et overblik over
-vores priser samt læse vores vilkår.</p>
-
-
-<p>Vi ønsker dig en god fornøjelse</p>
-
-<p>Med venlig hilsen<br/>
-<b>Finansmaskinen</b><br />
-<a href=\"$link\"> www.finansmaskinen.dk</a></p>
-
-
-<p style=\"font-size: 90%;\">Bemærk: Hvis du har modtaget denne meddelelse ved en fejl eller hvis du ikke ønsker
-at være bruger på finansmaskinen, så skal vi bede dig om blot at slette mailen. Tak.</p>";
-
+		return '<h1>Velkommen '.$this->user->name.'</h1>
+		<p>Din bruger på Finansmaskinen er blevet oprettet</p>
+		<p>Klik på nedenstående link for at aktivere din konto.<br />
+		Herefter vil du blive sendt igennem en lille opsætningsguide, og så er du kørende</p>
+		<a href="'.\config\config::$configs['finance']['settings']['protocol'].'://'.\config\config::$configs['finance']['domains']['web'].'/index/activate/'.$this->user->activationKey.'/'.$this->user->_id.'" style="">Aktiver</a>';
 	}
 	
 }
